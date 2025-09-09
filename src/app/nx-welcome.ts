413585d4c56588597674a9fdeb1a3c33c7ca44c1
Chien Phi Ba (DF-DC) <dc.ctb.chienpb@msb.com.vn>
2025-09-09 14:48:55 +0700

    fix: correct spelling of "Channel" in index.html and nx-welcome.ts

--- src/app/nx-welcome.ts
+++ src/app/nx-welcome.ts
@@ -6,14 +6,14 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div>
-      <h1> SEO MSB. Digibank Chanel</h1>
+      <h1> SEO MSB. Digibank Channel</h1>
       <h3>Hello, I'm Chris</h3>
       <div>Author is Chris | phibachien.</div>
       <div>
-        MSB Business Banking | Digibank Chanel | Chris | MSB | SMO | Chienpb
+        MSB Business Banking | Digibank Channel | Chris | MSB | SMO | Chienpb
       </div>
       <div>This is a web created by Angular Nx.</div>
-      <div>tag: msb-test-seo, phibachien, digibank chanel</div>
+      <div>tag: msb-test-seo, phibachien, digibank channel</div>
     </div>
   `,
   styles: [],
