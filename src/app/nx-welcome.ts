c99853b5e16db2cec0c8c494619943c5f4ba5805
Chien Phi Ba (DF-DC) <dc.ctb.chienpb@msb.com.vn>
2025-09-09 10:19:03 +0700

    update index.html to add additional favicon sizes and improve icon references

--- src/app/nx-welcome.ts
+++ src/app/nx-welcome.ts
@@ -6,7 +6,15 @@ import { CommonModule } from '@angular/common';
   imports: [CommonModule],
   template: `
     <div>
-      <h1>Hello, I'm Chris</h1>
+      <h1>Test SEO MSB. Just for test SEO.</h1>
+      <h3>Hello, I'm Chris</h3>
+      <div>Author is Chris.</div>
+      <div>
+        MSB Business Banking | Digibank Chanel | Chris | MSB | SMO | Chienpb |
+        msb-test-seo
+      </div>
+      <div>This is a web created by Angular Nx.</div>
+      <div>tag: msb-test-seo</div>
     </div>
   `,
   styles: [],
